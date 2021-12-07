93f934c0a47e91f660fa7b71a95d2a0a879bb9e6
itsnoproblem <noproblem@mulligan.xyz>
2021-12-06 23:57:28 -0500

    tools to verify contract + new / verified contract deployed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -28,4 +29,10 @@ module.exports = {
       gasPrice: 8000000000
     },
   },
+
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.REACT_APP_POLYGONSCAN_API_KEY
+  }
 };
